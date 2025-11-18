edc33e5325b0737415b4e49a815a1cc7f827afd3
SolicitedPoet2 <kedu.barros@gmail.com>
2025-11-18 12:34:37 -0300

    Site structure

--- beagames/src/components/Homepage/Homepage.jsx
+++ beagames/src/components/Homepage/Homepage.jsx
@@ -1,15 +1,31 @@
-import { useState } from 'react'
 import './Homepage.css'
 import logo from '../../assets/logo.webp';
+import menu from '../../assets/menu.svg'
+import TextField from "@mui/material/TextField";
 function HomePage() {
   return (
     <>
-      <main>
+      <div class="container-header">
         <header>
-          <img src={logo} alt="Logo" class="logo"/>
-          <nav>
-          </nav>
-        </header>
+            <nav>
+              <img src={menu} alt="Menu" class="menu header-items"/>
+              <h1>MENU</h1>
+            </nav>
+
+            <img src={logo} alt="Logo" class="logo header-items"/>
+            <div class="search">
+              <TextField
+                id="outlined-basic"
+                variant="outlined"
+                fullWidth
+                label="🔍 BUSCAR"
+              />
+            </div>
+          </header>
+      </div>
+   
+      <main>
+        
       </main>
     </>
   )
--- beagames/src/components/NotFound/NotFound.jsx
+++ beagames/src/components/NotFound/NotFound.jsx
@@ -1,15 +1,25 @@
 import { Link } from "react-router-dom"
+import TextField from "@mui/material/TextField";
 import './NotFound.css'
 
 function NotFound() {
   return (
     <>
-      <div>
-        <h1>Esta página não existe ❌</h1>
-        <Link to={"/"}>
-          <button>Volte para o homepage</button>
-        </Link>
-      </div>
+      <main class="notfound-main">
+        <div class="notfound">
+          <h1>Esta página não encontrada ❌</h1>
+          <div class="search">
+            <TextField
+              id="outlined-basic"
+              variant="outlined"
+              label="🔍 BUSCAR"
+            />
+          </div>
+          <Link to={"/"}>
+            <button>Volte para o homepage</button>
+          </Link>
+        </div>
+      </main>
     </>
   )
 }
